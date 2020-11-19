52a3358f6f70e17fc80892c9c8187731f27f7c7e
Davor Cortes <davorcortes.dc@gmail.com>
2020-11-19 15:43:53 -0500

    memoization

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -3,24 +3,22 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
     let answer = bigInt.zero;
+    let fibonacciSequence = new Map();
+    fibonacciSequence.set(0, bigInt.zero);
+    fibonacciSequence.set(1, bigInt.one);
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
+    const fibonacci = (n) =>{
+        if(fibonacciSequence.has(n)){
+            return fibonacciSequence.get(n);
         }
+        fibonacciSequence.set(n) = fibonacci(n - 1) + fibonacci(n-2);
+        return fibonacci(n);
     }
 
+    if (nth < 0) throw 'must be greater than 0';
+    answer = fibonacci(nth);
+    
     context.res = {
         body: answer.toString()
     };
