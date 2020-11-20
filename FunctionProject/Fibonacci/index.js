0c1342523641283a2d7b4cfcf1211ca1f6cb37bf
Davor Cortes <davorcortes.dc@gmail.com>
2020-11-19 21:58:32 -0500

    actualización de la función

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,25 +1,22 @@
-var bigInt = require("big-integer");
+let bigInt = require("big-integer");
+let fibonacciSequence = new Map();
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let nth = req.body.nth
+    let nth = req.body.nth;
     let answer = bigInt.zero;
-    let fibonacciSequence = new Map();
     fibonacciSequence.set(0, bigInt.zero);
     fibonacciSequence.set(1, bigInt.one);
 
-    const fibonacci = (n) =>{
-        if(fibonacciSequence.has(n)){
-            return fibonacciSequence.get(n);
-        }
-        fibonacciSequence.set(n) = fibonacci(n - 1) + fibonacci(n-2);
-        return fibonacci(n);
+    const fibonacci = n =>{
+        if(!fibonacciSequence.has(n)) fibonacciSequence.set(n, fibonacci(n-1).add(fibonacci(n-2)));
+        return fibonacciSequence.get(n);
     }
 
     if (nth < 0) throw 'must be greater than 0';
     answer = fibonacci(nth);
     
     context.res = {
-        body: answer.toString()
+        body: answer.toString();
     };
 }
